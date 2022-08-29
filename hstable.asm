6efa744465e3e16a90eb8570ac280e4a03548917
charlierobson <charlie.robson@b2m-solutions.com>
2022-08-29 11:34:32 +0100

    undo hs table vandalism, be more subtle

--- hstable.asm
+++ hstable.asm
@@ -1,37 +1,39 @@
 	.word	$89dc
-	.word	10
-	.asc	"PORTED FOR"
+	.word	16
+	.asc	"CHARLIE   "
+	.byte	0,0
+	.byte	2,0,2,2
+	
+	; .word	$89ec
+	; .word	10
+	; .asc	"          "
 
-	.word	$89ec
-	.word	10
-	.asc	"          "
+	; .word	$89fc
+	; .word	10
+	; .asc	"SORD M5 BY"
 
-	.word	$89fc
-	.word	10
-	.asc	"SORD M5 BY"
+	; .word	$8a0c
+	; .word	10
+	; .asc	"          "
 
-	.word	$8a0c
-	.word	10
-	.asc	"          "
+	; .word	$8a1c
+	; .word	10
+	; .asc	"SIR MORRIS"
 
-	.word	$8a1c
-	.word	10
-	.asc	"SIR MORRIS"
+	; .word	$8a2c
+	; .word	10
+	; .asc	"          "
 
-	.word	$8a2c
-	.word	10
-	.asc	"          "
+	; .word	$8a3c
+	; .word	10
+	; .asc	"-= 2022 =-"
+	; .word	$8a4c
 
-	.word	$8a3c
-	.word	10
-	.asc	"-= 2022 =-"
-	.word	$8a4c
-
-	.word	10
-	.asc	"          "
-	.word	$8a5c
-	.word	10
-	.asc	"          "
-	.word	$8a6c
-	.word	10
-	.asc	"SORRY GAIL"
+	; .word	10
+	; .asc	"          "
+	; .word	$8a5c
+	; .word	10
+	; .asc	"          "
+	; .word	$8a6c
+	; .word	10
+	; .asc	"SORRY GAIL"
