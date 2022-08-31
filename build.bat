582534604b0e12527ef3f478f58496e563ae6ce1
charlierobson <charlierobson@users.noreply.github.com>
2022-08-31 12:10:39 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ patcher CHUCKIEPADDED.BIN patch.bin chuckie.patched.bin
 if ERRORLEVEL 1 exit /b
 
 del /q m5cheg.bin
-brass m5cheg.asm m5cheg.bin -e -l m5cheg.html
+brass m5cheg.asm -e -l m5cheg.html
 
 copy /y m5cheg.bin C:\Users\robsonc\gh\mamebuild\roms\m5_cart\cheg\
 copy /y m5cheg.bin "C:\Users\robsonc\gh\m5multi\cart-binaries\sirmorris\Chuckie Egg.bin"
